7cd1350156256ecdc0fc428ceb7a9a1ea00171b2
abc33kr <abc33kr@gmail.com>
2014-03-18 16:00:10 -0400

    Revert "Movement alpha version"
    This reverts commit 13a7088c08f4176fa32b0d38dc3cfea22a185b8e.

--- Assets/TankAI.cs
+++ Assets/TankAI.cs
@@ -14,25 +14,15 @@ public class TankAI : MonoBehaviour {
 	public int tankHealth;
 	public int attackDamage = 1;
 
-	public Collider[] enemyTanks;
-	public Collider[] allyTanks;
+	private Collider[] enemyTanks;
+	private Collider[] allyTanks;
 	public Vector3 seekingPoint;
 
 	public int sightRange = 15;
 	public int coneBaseRadius = 30;
-
-	public int mass;
-	public Vector3 velocity;
-	public float maxVelocity;
-	public float acceleration;
-	public float slowingRadius;
-	public float haltingRadius;
-	private float wanderAngle;
 	
 	void Start () {
 		state = "Idle";
-		velocity = Vector3.zero;
-		wanderAngle = Vector3.Angle (velocity, new Vector3(0,1,0));
 	}
 
 	void Update () {
@@ -45,8 +35,8 @@ public class TankAI : MonoBehaviour {
 		Collider[] InRangeTanks = Physics.OverlapSphere (transform.position, sightRange);
 		//var Enemys = from tank in InRangeTanks where tank.tag != transform.tag select tank;
 		//var Allys = from tank in InRangeTanks where tank.tag == transform.tag select tank;
-		ArrayList Enemys = new ArrayList();//Collider[InRangeTanks.Length];
-		ArrayList Allys = new ArrayList();//[InRangeTanks.Length];
+		Collider[] Enemys = new Collider[InRangeTanks.Length];
+		Collider[] Allys = new Collider[InRangeTanks.Length];
 		//check cone
 		foreach (Collider tank in InRangeTanks) {
 			var targetLocalPosition = transform.InverseTransformPoint(tank.transform.position);		//this may not be right
@@ -58,30 +48,27 @@ public class TankAI : MonoBehaviour {
 			if (TargetXY.magnitude > coneRadius){
 				continue;
 			}
-			if(tank.tag == "Untagged"){
-				continue;
-			}
 			if(tank.tag == transform.tag){
-				Allys.Add(tank);
+				int allyLength = Allys.Length;
+				Allys[allyLength] = tank;
 			}
 			else{
-				Enemys.Add (tank);
+				int enemyLength = Enemys.Length;
+				Enemys[enemyLength] = tank;
 			}
 		}
 		//raycast
-		if(Enemys.Count != 0 && !state.Equals("Flee")){
+		if(Enemys.Length != 0 && !state.Equals("Flee")){
 			state = "Attack";
-			Collider enemy = (Collider)Enemys[0];
-			seekingPoint = enemy.transform.position;
+			seekingPoint = Enemys[0].transform.position;
 		}
-		if(Allys.Count != 0 && state.Equals("Idle")){
+		if(Allys.Length != 0 && state.Equals("Idle")){
 			state = "Follow";
-			Collider ally = (Collider)Allys[0];
-			seekingPoint = ally.transform.position;
+			seekingPoint = Allys[0].transform.position;
 		}
 		if(seekingPoint == transform.position && state == "Follow")
 			state = "Idle";
-		if(state.Equals("Attack") && Enemys.Count == 0){
+		if(state.Equals("Attack") && Enemys.Length == 0){
 			state = "Follow";
 		}
 		//else{
@@ -89,7 +76,7 @@ public class TankAI : MonoBehaviour {
 	}
 	void Move(){
 
-		 if (state == "Idle")
+		 if (state == "idle")
 		  		Wander();
 		 if (state == "Attack")
 		  		AttackMove();
@@ -100,102 +87,17 @@ public class TankAI : MonoBehaviour {
 	}
 
 	void Wander(){
-
-		// Calculate the circle center
-		Vector3 circleCenter;
-		circleCenter = velocity;
-		Vector3.Normalize(circleCenter);
-		Vector3.Scale(circleCenter, new Vector3(5,0,5));
-		// Calculate the displacement force
-		Vector3 displacement;
-		displacement = new Vector3(0, 0, 1);
-
-		// Randomly change the vector direction
-		// by making it change its current angle
-		displacement = SetAngle(displacement, wanderAngle);
-
-		// Change wanderAngle just a bit, so it
-		// won't have the same value in the
-		// next game frame.
-		//
-		// Finally calculate and return the wander force
-		Vector3 wanderForce;
-		wanderAngle += Random.Range(0f, 1f) * .05f - .05f * .5f;
-		wanderForce = circleCenter + displacement;
-
-		wanderForce = Vector3.	ClampMagnitude(wanderForce, acceleration);
-		wanderForce = wanderForce / mass;
-		velocity = Vector3.ClampMagnitude((velocity + wanderForce), maxVelocity);
-		transform.Translate(velocity);
-		if (velocity.magnitude >= .2f || velocity.magnitude <= -.2f) {
-			transform.forward = velocity;
-		}
-	}
-
-	Vector3 SetAngle(Vector3 vector, float value){
-		float len = vector.magnitude;
-		vector.x = Mathf.Cos(value) * len;
-		vector.z = Mathf.Sin(value) * len;
-		return vector;
 	}
 
 	void AttackMove(){
-		//courtesy of http://gamedevelopment.tutsplus.com/tutorials/understanding-steering-behaviors-flee-and-arrival--gamedev-1303
-		Vector3 desiredVelocity = (seekingPoint - transform.position).normalized;
-		float distance = Vector3.Distance (seekingPoint, transform.position);
-		if (distance > slowingRadius) {
-			desiredVelocity = new Vector3 (desiredVelocity.x * maxVelocity, 0f, desiredVelocity.z * maxVelocity);
-		} 
-		if (distance <= slowingRadius && distance > haltingRadius) {
-			Vector3.Scale(desiredVelocity, new Vector3(maxVelocity * distance/ slowingRadius, 0f, maxVelocity * distance/ slowingRadius));
-		}
-		if (distance <= haltingRadius) {
-			desiredVelocity = new Vector3(0,0,0);
-		}
-		Vector3 steering = desiredVelocity - velocity;
-		
-		steering = Vector3.	ClampMagnitude(steering, acceleration);
-		steering = steering / mass;
-		
-		velocity = Vector3.ClampMagnitude((velocity + steering), maxVelocity);
-		transform.Translate(velocity);
-		if (velocity.magnitude >= .2f || velocity.magnitude <= -.2f) {
-			transform.forward = velocity;
-		}
 
 		Aim ();
 	}
 
 	void Flee(){
-		//courtesy of http://gamedevelopment.tutsplus.com/tutorials/understanding-steering-behaviors-flee-and-arrival--gamedev-1303
-		Vector3 desiredVelocity = (transform.position-seekingPoint).normalized;
-		desiredVelocity = new Vector3 (desiredVelocity.x*maxVelocity, 0f, desiredVelocity.z*maxVelocity);
-		Vector3 steering = desiredVelocity - velocity;
-		
-		steering = Vector3.	ClampMagnitude(steering, acceleration);
-		steering = steering / mass;
-		
-		velocity = Vector3.ClampMagnitude((velocity + steering), maxVelocity);
-		transform.Translate(velocity);
-		if (velocity.magnitude >= .2f || velocity.magnitude <= -.2f) {
-			transform.forward = velocity;
-		}
 	}
 
 	void Seek(){
-		//courtesy of http://gamedevelopment.tutsplus.com/tutorials/understanding-steering-behaviors-seek--gamedev-849
-		Vector3 desiredVelocity = (seekingPoint - transform.position).normalized;
-		desiredVelocity = new Vector3 (desiredVelocity.x*maxVelocity, 0f, desiredVelocity.z*maxVelocity);
-		Vector3 steering = desiredVelocity - velocity;
-
-		steering = Vector3.	ClampMagnitude(steering, acceleration);
-		steering = steering / mass;
-				
-		velocity = Vector3.ClampMagnitude((velocity + steering), maxVelocity);
-		transform.Translate(velocity);
-		if (velocity.magnitude >= .2f || velocity.magnitude <= -.2f) {
-			transform.forward = velocity;
-		}
 	}
 
 	void Aim(){
