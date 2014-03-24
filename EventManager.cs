df5d38bb4f90b907bbe9511647d60871a7bbf07f
chhf129 <hxc334@case.edu>
2014-03-24 15:19:07 -0400

    updated: moving sound

--- EventManager.cs
+++ EventManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 [RequireComponent(typeof(AudioSource))]
-public class NewBehaviourScript : MonoBehaviour {
+public class EventManager : MonoBehaviour {
 	public GameObject allyTankPrefab,playerPrefab,enemyTankPrefab;
 	public GameObject exclamationMark;
 	public AudioClip shootSound,moveSound;
@@ -10,7 +10,7 @@ public class NewBehaviourScript : MonoBehaviour {
 	//Instantiate tanks, i think player tank better be manually placed on scene
 	void Start () {
 		GameObject temp;
-	    //	temp = (GameObject)Instantiate (playerPrefab, new Vector3 (3, 5, 41), Quaternion.identity);
+	    //	temp = (GameObject)Instantiate (playerPrefab, new Vector3 (12, 5, 164), Quaternion.identity);
 		//temp.tag="Player";
 		temp=(GameObject)Instantiate (allyTankPrefab, new Vector3 (12, 3, 60), Quaternion.identity);
 		temp.tag = "Ally";
@@ -33,15 +33,19 @@ public class NewBehaviourScript : MonoBehaviour {
 	}
 
 	void Update () {
-		//Debug.Log (Time.deltaTime);
 		moveSoundDelay -= Time.deltaTime;
+		//player sound play
+		if ((Input.GetButton ("Horizontal") || Input.GetButton ("Vertical")))
+						PlayMoveSound ();
+				else
+						audio.Stop ();
 	}
 
 	//call under "move" method
 	void PlayMoveSound(){
 		if (moveSoundDelay <= 0) {
 			audio.PlayOneShot (moveSound);
-			moveSoundDelay = 10f;
+			moveSoundDelay = 1.565f;
 		}
 		HearDetection (40f);
 	}
@@ -52,13 +56,14 @@ public class NewBehaviourScript : MonoBehaviour {
 		HearDetection (80f); //shooting have much larger hearRange than moving 
 	}
 
+
 	//detect enemy tank within certain range and tell them that "i am here" when playing any sound, and enemy tank will come to position last time they hear sound 
 	void HearDetection(float hearRange){
 		Collider[] InRangeTanks = Physics.OverlapSphere (transform.position, hearRange);
 		foreach (Collider tank in InRangeTanks)
-						if (tank.tag == "Enemy") {
+						if (tank.tag == "Ally") {
 								//idea: instantiate exclamationMark on those enemy tank to show their alert
-								Destroy (Instantiate (exclamationMark, tank.transform.position + Vector3.up, Quaternion.identity), 4f);
+								Destroy (Instantiate (exclamationMark, tank.transform.position + 5*Vector3.up, Quaternion.identity), 3f);
 								// i am not sure about this part
 								if (tank.GetComponent<TankAI> ().state == "Idle") {
 										tank.GetComponent<TankAI> ().state = "follow";
